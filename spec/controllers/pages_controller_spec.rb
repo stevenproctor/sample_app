77fbeb8e2b2f8d8be0487a7301c53e7472996175
Steven Proctor <steven.proctor@gmail.com>
2011-04-30 12:28:57 -0500

    Refactoring as part of section 3.5 of Rails Tutorial.  Modeled after Uncle Bob Martin's prime factors kata example

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,43 +3,22 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
+  [
+    ['home', 'Home'],
+    ['contact', 'Contact'],
+    ['about', 'About']
+  ].each do |action, pageName|
+    describe "GET '#{action}'" do
+      it "should be successful" do
+        get "#{action}"
+        response.should be_success
+      end
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
-
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Contact")
+      it "should have the right title" do
+        get "#{action}"
+        response.should have_selector("title",
+                                     :content => "Ruby on Rails Tutorial Sample App | #{pageName}")
+      end
     end
   end
-
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | About")
-    end
-  end
-
 end
