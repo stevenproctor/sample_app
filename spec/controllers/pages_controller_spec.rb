f535aead09e5aa935930c8992371eb9dceb58ee9
Steven Proctor <steven.proctor@gmail.com>
2011-04-30 12:34:55 -0500

    Refactored out the GET for the actions

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,13 +9,15 @@ describe PagesController do
     ['about', 'About']
   ].each do |action, pageName|
     describe "GET '#{action}'" do
-      it "should be successful" do
+      before(:each) do
         get "#{action}"
+      end
+
+      it "should be successful" do
         response.should be_success
       end
 
       it "should have the right title" do
-        get "#{action}"
         response.should have_selector("title",
                                      :content => "Ruby on Rails Tutorial Sample App | #{pageName}")
       end
