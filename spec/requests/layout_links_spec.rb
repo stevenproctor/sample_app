e9c6874b48556097f1de4a6d3af46afb3cf708b6
Steven Proctor <steven.proctor@gmail.com>
2011-05-02 19:46:33 -0500

    Rails Tutorial section 5.5 Exercises

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -14,4 +14,18 @@ describe "LayoutLinks" do
       response.should have_selector('title', :content=>page_name)
     end
   end
+
+  [
+    ['About', 'About'],
+    ['Help', 'Help'],
+    ['Contact', 'Contact'],
+    ['Home', 'Home'],
+    ['Sign up now!', 'Sign Up']
+  ].each do |link_text, page_name|
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link link_text
+      response.should have_selector('title', :content => page_name)
+    end
+  end
 end
