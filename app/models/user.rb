0af30865f7096e71076a942f081348ab72341d37
Steven Proctor <steven.proctor@gmail.com>
2011-05-06 21:03:29 -0500

    Tested out Tabify on user.rb, and updated to filter out log files from git

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
 
   email_regex = /\A[\w\d+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  :presence => true,
-                    :length => {:maximum => 50 }
-  validates :email, :presence => true,
-                    :format => { :with => email_regex },
+  validates :name,  :presence   => true,
+                    :length     => {:maximum        => 50 }
+  validates :email, :presence   => true,
+                    :format     => {:with           => email_regex },
                     :uniqueness => {:case_sensitive => false }
 end
