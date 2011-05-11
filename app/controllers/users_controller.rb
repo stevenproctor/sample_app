6b5d06c422b8cf9df8b763382b47d50f46dce23a
Steven Proctor <steven.proctor@gmail.com>
2011-05-11 18:44:59 -0500

    Added failure user case for User Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
     @title = @user.name
   end
   def new
+    @user = User.new
     @title = "Sign Up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #handle a successful save
+    else
+      @title = "Sign up"
+      render :new
+    end
   end
 
 end
