a8f711991a54821e6569d5a21e1e6c0c60fc8517
Steven Proctor <steven.proctor@gmail.com>
2011-05-11 18:22:22 -0500

    Cleanup UsersControllers test for GET 'new'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,13 +4,15 @@ describe UsersController do
   render_views
 
   describe "GET 'new'" do
+    before(:each) do
+      get :new
+    end
+
     it "should be successful" do
-      get 'new'
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'new'
       response.should have_selector("title", :content => "Sign Up")
     end
   end
@@ -18,30 +20,26 @@ describe UsersController do
   describe "GET 'show'" do
     before(:each) do
       @user = Factory(:user)
+      get :show, :id => @user
     end
 
     it "should be successful" do
-      get :show, :id => @user
       response.should be_success
     end
 
     it "should find the right user" do
-      get :show, :id => @user
       assigns(:user).should == @user
     end
 
     it "should have the right title" do
-      get :show, :id => @user
       response.should have_selector("title", :content => @user.name)
     end
 
     it "should include the user's name" do
-      get :show, :id => @user
       response.should have_selector("h1", :content => @user.name)
     end
 
     it "should have a profile image" do
-      get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
